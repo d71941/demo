ef2dfe447381fb713ebe50e8e16f3e6c3d05b98f
d71941 <d71941@gmail.com>
2015-05-12 21:54:39 +0800

    ...

--- common.js
+++ common.js
@@ -2,33 +2,44 @@ function initCanvas(name) {
 	var canvas = document.getElementById(name);
 	var context = canvas.getContext('2d');
 
-	context.canvas.width  = window.innerWidth;
-	context.canvas.height = window.innerHeight - parseInt($('#viewport').css('top'));
+	context.canvas.width  = $("#baseImage").width();
+	context.canvas.height = $("#baseImage").height();
 }
 
-function addImage(canvasName, imagePath) {
-	var canvas = document.getElementById(canvasName);
-	var context = canvas.getContext('2d');
+function loadImage(imagePath) {
 	var imageObj = new Image();
 
 	imageObj.onload = function() {
-		windowRatio = context.canvas.width / context.canvas.height;
+		var topOffset = 40
+		windowWidth = window.innerWidth;
+		windowHeight = window.innerHeight - topOffset;
+		windowRatio = windowWidth / windowHeight;
 		imageRatio = imageObj.width / imageObj.height;
 
 		if(windowRatio > imageRatio) {
-			height = context.canvas.height;
+			height = windowHeight;
 			width = height * imageRatio;
-			x = (context.canvas.width - width) / 2;
+			x = (windowWidth - width) / 2;
 			y = 0;
 		}
 		else {
-			width = context.canvas.width;
+			width = windowWidth;
 			height = width / imageRatio;
 			x = 0;
-			y = (context.canvas.height - height) / 2;
+			y = (windowHeight - height) / 2;
 		}
 
-		context.drawImage(imageObj, x, y, width, height);
+		x = parseInt(x);
+		y = parseInt(y);
+		width = parseInt(width);
+		height = parseInt(height);
+
+		$("#baseImage").width(width);
+		$("#baseImage").height(height);
+		$("#viewport").css('left', x);
+		$("#viewport").css('top', y + 40);
+		$("#baseImage").attr('src', imagePath);
+		initCanvas('polygonCanvas');
 	};
 	imageObj.src = imagePath;
 }
--- question.js
+++ question.js
@@ -16,3 +16,111 @@ function addVertexToContainer(x, y){
     stop: updateVertex
   });
 }
+
+function drawPoly(){
+  var canvas = document.getElementById("polygonCanvas");
+  var context = canvas.getContext("2d");
+
+  context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+
+  for(var polygonIndex in polygons) {
+    context.beginPath();
+    var first = true;            
+    for(var vertexIndex in polygons[polygonIndex].vertices) {
+      var position = polygons[polygonIndex].vertices[vertexIndex];
+
+      if (first) {
+        context.moveTo(position.x, position.y);
+        first = false;
+      } else {
+        context.lineTo(position.x, position.y);
+      }
+    }
+    context.closePath();
+    context.fillStyle = "rgba(100, 100, 100, 0.5)";
+    context.fill();
+    if(polygonIndex == currentPolygonIndex) {
+      context.strokeStyle = "#FF0000";
+      context.stroke(); 
+    }
+  }
+}
+
+function moveBack(){
+  if(currentPolygonIndex <= 0) return;
+  var tmpPolygon = polygons[currentPolygonIndex];
+
+  polygons[currentPolygonIndex] = polygons[currentPolygonIndex - 1];
+  $('#polygonButtonContainer .polygonButton').eq(currentPolygonIndex).removeClass('currentPolygonButton')
+
+  currentPolygonIndex = currentPolygonIndex - 1;
+
+  polygons[currentPolygonIndex] = tmpPolygon;
+  $('#polygonButtonContainer .polygonButton').eq(currentPolygonIndex).addClass('currentPolygonButton')
+}
+
+function moveForward(){
+  if(currentPolygonIndex >= polygons.length - 1) return;
+  var tmpPolygon = polygons[currentPolygonIndex];
+
+  polygons[currentPolygonIndex] = polygons[currentPolygonIndex + 1];
+  $('#polygonButtonContainer .polygonButton').eq(currentPolygonIndex).removeClass('currentPolygonButton')
+
+  currentPolygonIndex = currentPolygonIndex + 1;
+
+  polygons[currentPolygonIndex] = tmpPolygon;
+  $('#polygonButtonContainer .polygonButton').eq(currentPolygonIndex).addClass('currentPolygonButton')
+}
+
+var polygons = [];
+var currentPolygonIndex = -1;
+
+$(function() {
+
+  var canvas = document.getElementById("polygonCanvas");
+  var context = canvas.getContext("2d");
+
+  canvas.addEventListener('click', function(e){
+    if(currentPolygonIndex == -1) return;
+    vertices = polygons[currentPolygonIndex].vertices;
+    vertices[vertices.length] = {x: e.clientX - parseInt($('#viewport').css('left')), y: e.clientY - parseInt($('#viewport').css('top'))};
+    addVertexToContainer(vertices[vertices.length - 1].x, vertices[vertices.length - 1].y);
+    drawPoly();
+  });
+
+  $("#addPolygon").click(function(){
+    currentPolygonIndex = polygons.length;
+    polygons[currentPolygonIndex] = {vertices:[]};
+    $("#polygonButtonContainer .currentPolygonButton").removeClass('currentPolygonButton');
+    $('#polygonButtonContainer').append('<button class="polygonButton currentPolygonButton" polygonIndex='+currentPolygonIndex+' type="button">'+currentPolygonIndex+'</button>')
+    $("#polygonButtonContainer .polygonButton:last-child").click(function(){
+      if(currentPolygonIndex == parseInt($(this).attr('polygonIndex'))) return;
+      currentPolygonIndex = parseInt($(this).attr('polygonIndex'));
+      $("#polygonButtonContainer .currentPolygonButton").removeClass('currentPolygonButton');
+      $(this).addClass('currentPolygonButton');
+      $("#vertexContainer").empty();
+      for(var vertexIndex in polygons[currentPolygonIndex].vertices) {
+        var position = polygons[currentPolygonIndex].vertices[vertexIndex];
+        addVertexToContainer(position.x, position.y);
+      }
+      drawPoly();
+    });
+    $("#vertexContainer").empty();
+    drawPoly();
+  });
+
+  $("#removePolygon").click(function (){
+    if(currentPolygonIndex == -1) return;
+    polygons.splice(currentPolygonIndex, 1);
+    $("#polygonButtonContainer .polygonButton:last-child").remove();
+    currentPolygonIndex = -1;
+    $("#polygonButtonContainer .currentPolygonButton").removeClass('currentPolygonButton');
+    $("#vertexContainer").empty();
+    drawPoly();
+  });
+
+  $("#moveBack").click(moveBack);
+  $("#moveForward").click(moveForward);
+
+  loadImage("question.jpg");
+});
