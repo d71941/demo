ed1e70bdf723ce4f7440fcc47b4ddc56a5187d46
d71941 <d71941@gmail.com>
2015-05-14 00:41:21 +0800

    Add sever

---
+++ server/server.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask import request
+from flask import json
+
+STATIC_FOLDER = '../client'
+
+app = Flask(__name__,static_path = '', static_folder = STATIC_FOLDER)
+
+@app.route('/update_question', methods=['POST'])
+def update_question():
+	f = open(STATIC_FOLDER + '/question.json', 'w')
+	f.write(json.dumps(request.get_json(force=True)))
+	return "ok"
+
+if __name__ == '__main__':
+    app.run(debug=True)
